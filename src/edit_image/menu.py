e37ff7d0bf7590fb40471526d3141777d4df18d6
Sujal Singh <email.sujalsingh@gmail.com>
2021-08-08 11:21:44 +0530

    edit menu partially working

---
+++ src/edit_image/menu.py
@@ -0,0 +1,39 @@
+import easygui
+
+
+def edit_menu():
+    while True:
+        print('''
+    ╔═╦═╦═╦══╦══╗        ╔══╦═╦═╗
+    ║╬║╬║╦╣══╣══╣        ║═╦╣║║╬║
+    ║╔╣╗╣╩╬══╠══║        ║╔╝║║║╗╣
+    ╚╝╚╩╩═╩══╩══╝        ╚╝─╚═╩╩╝
+
+         1              adjust brightness
+         2              change contrast
+         3              adding filters
+         4              resizing the image
+         5              crop the image
+         6              go back to main menu ''')
+
+        edit_choice = input("ENTER CHOICE: ")
+        print("CHOOSE FILE PATH: ")
+        file_path = easygui.fileopenbox()
+
+        if edit_choice == "1":
+            adjust_birghtness(file_path)
+        elif edit_choice == "2":
+            change_contrast(file_path)
+        elif edit_choice == "3":
+            add_filters(file_path)
+        elif edit_choice == "4":
+            resize_img(file_path)
+        elif edit_choice == "5":
+            crop_img(file_path)
+        elif edit_choice == "6":
+            break
+        else:
+            print('\033[31m' + 'OPTION NOT FOUND' + '\033[0m')
+
+
+edit_menu()
