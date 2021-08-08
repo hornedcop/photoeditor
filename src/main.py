342ac20988dda5388a74dcdcf7ebde8a961db604
hornedcop <sarthakmishra165@gmail.com>
2021-08-08 10:02:43 +0530

    menu added

--- src/main.py
+++ src/main.py
@@ -1,1 +1,42 @@
-# MAIN ENTRYPOINT INTO THE PROGRAM
+username = "admin"
+
+
+def random_img():
+    return
+
+
+def edit():
+    return
+
+
+def menu():
+    # greeting
+    print('おはよ ございます',
+          f"GOOD MORNING, {username}",
+          "Let's start editing", sep='\n')
+
+    while True:
+        print('''
+ _____ _____ _____ _____ _____                        _____ _____ _____ 
+|  _  | __  |   __|   __|   __|                      |   __|     | __  |
+|   __|    -|   __|__   |__   |                      |   __|  |  |    -|
+|__|  |__|__|_____|_____|_____|                      |__|  |_____|__|__|
+
+        1                                               Edit a photo
+        2                                               Generate a random image
+        3                                               EXIT ''')
+
+        choice = int(input("Enter your choice : "))
+        if choice == 1:
+            edit()
+        elif choice == 2:
+            random_img()
+        elif choice == 3:
+            print('Sayōnara')
+            break
+
+        else:
+            print('\033]31m' + 'OPTION NOT FOUND' + '\033]0m')
+
+
+menu()
