6fa80d4f8f34437001afe8bfe4563b937e2b6ac1
Sujal Singh <email.sujalsingh@gmail.com>
2021-08-16 08:50:01 +0530

    contrast operation complete

--- src/edit_image/image_operations.py
+++ src/edit_image/image_operations.py
@@ -29,7 +29,28 @@ def adjust_brightness(file_path):
 
 
 def change_contrast(file_path):
-    return
+    img = Image.open(file_path)  # CREATES AN IMAGE OBJECT
+    edited_img = Enhance.Contrast(img)  # CREATES AN IMAGE OBJECT WHOSE CONTRAST CAN BE CHANGED
+
+    print("HINT: 0 AS CONTRAST FACTOR GIVES A GREY IMAGE",
+          "      1 AS CONTRAST FACTOR GIVES ORIGINAL IMAGE",
+          "      NEGATIVE VALUES INVERT THE IMAGE", sep="\n")
+
+    while True:
+        try:
+            factor = float(input('Enter the Contrast factor : '))
+            break
+        except ValueError:
+            print('\033[31m' + 'INVALID INPUT' + '\033[0m')
+
+    edited_img = edited_img.enhance(factor)
+
+    if input("Do you want to see this image (y/n): ").lower() == "y":
+        edited_img.show()
+    if input("Do you want to save this image (y/n): ").lower() == "y":
+        new_file = auto_renamed_writable_file(file_path)
+        edited_img.save(new_file)
+        new_file.close()
 
 
 def add_filters(file_path):
